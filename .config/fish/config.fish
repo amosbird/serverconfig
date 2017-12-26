d1ab8c4f80890d3dc26182c3f7f888b297db2ef7
Amos Bird <amosbird@gmail.com>
2017-12-26 10:55:46 +0800

    add /usr/local/bin /usr/bin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,10 +8,6 @@ set FZF_HOME $HOME/.fzf
 # end
 
 set -x RUST_SRC_PATH /home/amos/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
-# set -x TERMINFO $HOME/share/terminfo
-set -x C_INCLUDE_PATH /home/amos/include $C_INCLUDE_PATH
-set -x CPLUS_INCLUDE_PATH /home/amos/include $CPLUS_INCLUDE_PATH
-set -x LIBRARY_PATH /home/amos/lib $LIBRARY_PATH
 set -x FZF_DEFAULT_OPTS "--ansi --multi --bind=ctrl-v:half-page-down,alt-v:half-page-up,ctrl-l:accept"
 set -x GOPATH $HOME/go
 set -x CARGO_HOME $HOME/.cargo
@@ -25,7 +21,7 @@ set -x QT_IM_MODULE fcitx
 set -x ASAN_OPTIONS "handle_segv=0:detect_leaks=0"
 set -x USE_GOLD_LINKER true
 set -x DIRENV_LOG_FORMAT ""
-set -x FLAMEGRAPH_DIR /home/amos/softwares/FlameGraph
+set -x FLAMEGRAPH_DIR /home/amos/git/FlameGraph
 set -x UID (id -u)
 
 status --is-interactive;
@@ -63,7 +59,7 @@ end
 # end
 
 # prepend path
-set -U fish_user_paths $HOME/scripts $FZF_HOME/bin $GOPATH/bin $CARGO_HOME/bin $CABAL_HOME/bin $HOME/.local/bin $HOME/bin /bin /sbin
+set -U fish_user_paths $HOME/scripts $FZF_HOME/bin $GOPATH/bin $CARGO_HOME/bin $CABAL_HOME/bin $HOME/.local/bin $HOME/bin /usr/local/bin /usr/bin /bin /sbin
 
 if test -e ~/.local/share/icons-in-terminal/icons.fish
     source ~/.local/share/icons-in-terminal/icons.fish
