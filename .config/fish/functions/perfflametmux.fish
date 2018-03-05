d6db2ca67b48985fbb22bdf2a1a83c58a32e5251
Amos Bird <amosbird@gmail.com>
2018-03-05 23:55:23 +0800

    fix tmux.

--- .config/fish/functions/perfflametmux.fish
+++ .config/fish/functions/perfflametmux.fish
@@ -1,4 +1,4 @@
-function perfflame
+function perfflametmux
     # set -l options -x 'f,F' -x 'F,s' 'h/help' 'f/file' 'F/fifo' 's/suffix=' 'T-testing'
     # argparse -n psub --max-args=0 $options -- $argv
     argparse -n perfflame 'h/help' 'p=' -- $argv
@@ -19,15 +19,13 @@ function perfflame
         set -l centercol (math \({$cols}-(string length $msg)\)/2)
         set -l centercol2 (math \({$cols}-8\)/2)
         set -l centerrow (math $rows/2)
-        tput smcup
         tput cup $centerrow $centercol
         set -l msg \e\[1m\e\[92m$msg\e\[0m
         set -l clock \e\[1m\e\[33m(string repeat -n $centercol2 " ")
         echo $msg
         echo
-        # need time to resume execution, hacky
-        time sw $clock $df $ds perf record -F 99 -g -p $_flag_p -o $df ^ /dev/null
-        tput rmcup
+        sw $clock $df $ds perf record -F 99 -g -p $_flag_p -o $df ^ /dev/null
+        # warning: this never runs. The script should be driven by tmux
         popd
     end
 end
