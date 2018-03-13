08dc745f77785a05bf5dbdeae9980bd3b0b5f9b4
Amos Bird <amosbird@gmail.com>
2018-03-13 17:31:08 +0800

    start emacs in daemon.

--- scripts/g201.sh
+++ scripts/g201.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-urxvt -T 201.nobida.cn -e ssh -t 201.nobida.cn "tmux new -d -s htop htop; tmux new -d -s emacs bash -l -c 'emacs -nw'; tmux new-session -A -s amos"
+urxvt -T 201.nobida.cn -e ssh -t 201.nobida.cn "tmux new -d -s htop htop; tmux new -d -s emacs bash -l -c \"emacsclient -a '' -t -c\"; tmux new-session -A -s amos"
