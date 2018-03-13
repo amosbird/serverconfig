c6fcf083eb7f0e4b5cc37565ddecbfd921682d56
Amos Bird <amosbird@gmail.com>
2018-03-13 13:42:34 +0800

    new config.

--- .config/fish/functions/osc52scp.fish
+++ .config/fish/functions/osc52scp.fish
@@ -14,7 +14,7 @@ function osc52scp -d "scp file via osc52" --argument-names 'file'
     set esc "\033]52;y;"(printf $buf | head -c $max | base64 | tr -d '\r\n')"\07"
     set esc "\033Ptmux;\033"$esc"\033\\" # tmux
     printf "$esc"
-    osc52browser "file:///tmp/"(basename $path)
+    osc52browser "/tmp/"(basename $path)
   else
     echo "File $file is not an existing file or directory."
     return 1
