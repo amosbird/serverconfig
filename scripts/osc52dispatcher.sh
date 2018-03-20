74e2dd45293bb01d489fd1e091b8b271d3357f55
Amos Bird <amosbird@gmail.com>
2018-03-21 01:10:19 +0800

    i3 move back.

--- scripts/osc52dispatcher.sh
+++ scripts/osc52dispatcher.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 case "$1" in
-    x) luakit $2
+    x) luakit "$2"
        ;;
-    y) vivaldi $2
+    y) vivaldi "$2"
+       ;;
+    z) i3-msg focus "$2"
        ;;
     *) $2
        ;;
