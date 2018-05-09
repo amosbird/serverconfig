3615c9ee3fabb883e855eaab0ee62b6553393ca3
Amos Bird <amosbird@gmail.com>
2018-05-09 11:36:46 +0800

    99.92%

--- restore.sh
+++ restore.sh
@@ -35,8 +35,6 @@ rm -rf "$HOME/.tmux.conf"
 ln -sf "$DIR/.tmux" "$HOME"/
 ln -sf "$DIR/.tmux/.tmux.conf" "$HOME"/
 
-ln -sf "$DIR/init.el" "$HOME"/.emacs.d/
-
 sudo cp -r "$DIR"/xkb/* /usr/share/X11/xkb/symbols/
 
 setxkbmap us
