a8562cf786af6e3d018f0d67a4abf1a7e6e3fa16
Amos Bird <amosbird@gmail.com>
2018-08-16 18:56:38 +0800

    better.

--- scripts/i123.sh
+++ scripts/i123.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # urxvt -T 10.61.2.123 -e ssh -t 10.61.2.123 /home/amos/scripts/tstart.sh
-alacritty -t 10.61.2.123 -e ssh -t 10.61.2.123 'bash -l /home/amos/scripts/tstart.sh'
+# alacritty -t 10.61.2.123 -e ssh -t 10.61.2.123 'bash -l /home/amos/scripts/tstart.sh'
 # st -T 10.61.2.123 -e ssh -t 10.61.2.123 /home/amos/scripts/tstart.sh
 
 # need to installterm first
-# termite -t 10.61.2.123 -e "ssh -t 10.61.2.123 /home/amos/scripts/tstart.sh"
+termite -t 10.61.2.123 -e "ssh -t 10.61.2.123 /home/amos/scripts/tstart.sh"
