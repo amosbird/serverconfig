f37149517298cd0d98fee7369ef4c9aae26e9a58
Amos Bird <amosbird@gmail.com>
2018-10-26 00:18:37 +0800

    done 4

--- .config/fish/functions/__fish_stop_bracketed_paste.fish
+++ .config/fish/functions/__fish_stop_bracketed_paste.fish
@@ -3,9 +3,7 @@ function __fish_stop_bracketed_paste
 	set fish_bind_mode $__fish_last_bind_mode
     set -e __fish_paste_quoted
     commandline | read -z cmdline
-    string trim -- $cmdline | read -z cmdline
-    echo $cmdline > /tmp/cmd
-    test -z "$cmdline[-1]" && set -e cmdline[-1]
+    string trim -- $cmdline | perl -pe 'chomp if eof' | read -z cmdline
     set -l x (string sub -l $__fish_amos_cursor $__fish_amos_cmd)
     set -l y (string sub -s (math $__fish_amos_cursor + 1) $__fish_amos_cmd)
     commandline -r "$x$cmdline$y"
