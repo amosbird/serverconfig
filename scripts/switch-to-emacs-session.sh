a207b1462172daf92f89a25bca225250074714a4
Amos Bird <amosbird@gmail.com>
2019-02-19 18:30:43 +0800

    fix dangling emacs daemon

--- scripts/switch-to-emacs-session.sh
+++ scripts/switch-to-emacs-session.sh
@@ -2,6 +2,7 @@
 
 if ! tmux list-sessions | grep -q -F emacs
 then
+    pkill -f "emacs --daemon"  # sometimes emacs daemon doesn't quit
     tmux new -d -s emacs fish -i -c "emacsclient -a '' -t -c";
 fi
 tmux switch-client -t emacs
