6089f4c4ec685d772c19907e81a7ced9628ee7a2
Amos Bird <amosbird@gmail.com>
2019-03-26 01:31:45 +0800

    add universal variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -51,8 +51,38 @@ end)
 status --is-interactive;
 and source $ASDF_DIR/completions/asdf.fish
 
-# prepend path
-set -U fish_user_paths $HOME/scripts $ASDF_DIR/bin $ASDF_DIR/shims $asdf_data_dir/shims $FZF_HOME/bin $GOPATH/bin $CARGO_HOME/bin $CABAL_HOME/bin $ARCANIST_HOST/bin $HOME/.local/bin $HOME/bin /usr/local/bin /usr/local/go/bin /usr/bin /bin /sbin /usr/sbin
+if not set -q fish_initialized
+    set -U fish_color_autosuggestion '555'  'brblack'
+    set -U fish_color_cancel -r
+    set -U fish_color_comment red
+    set -U fish_color_cwd green
+    set -U fish_color_cwd_root red
+    set -U fish_color_end brmagenta
+    set -U fish_color_error brred
+    set -U fish_color_escape 'bryellow'  '--bold'
+    set -U fish_color_history_current --bold
+    set -U fish_color_host normal
+    set -U fish_color_match --background=brblue
+    set -U fish_color_normal normal
+    set -U fish_color_operator bryellow
+    set -U fish_color_param cyan
+    set -U fish_color_quote yellow
+    set -U fish_color_redirection brblue
+    set -U fish_color_search_match 'bryellow'  '--background=brblack'
+    set -U fish_color_selection 'white'  '--bold'  '--background=brblack'
+    set -U fish_color_status red
+    set -U fish_color_user brgreen
+    set -U fish_color_valid_path --underline
+    set -U fish_key_bindings fish_default_key_bindings
+    set -U fish_pager_color_completion
+    set -U fish_pager_color_description 'B3A06D'  'yellow'
+    set -U fish_pager_color_prefix 'white'  '--bold'  '--underline'
+    set -U fish_pager_color_progress 'brwhite'  '--background=cyan'
+
+    # prepend path
+    set -U fish_user_paths $HOME/scripts $ASDF_DIR/bin $ASDF_DIR/shims $asdf_data_dir/shims $FZF_HOME/bin $GOPATH/bin $CARGO_HOME/bin $CABAL_HOME/bin $ARCANIST_HOST/bin $HOME/.local/bin $HOME/bin /usr/local/bin /usr/local/go/bin /usr/bin /bin /sbin /usr/sbin
+    set -U fish_initialized
+end
 
 contains -- /usr/share/fish/vendor_completions.d $fish_complete_path;
 or set -g fish_complete_path $fish_complete_path[1..-2] /usr/share/fish/vendor_completions.d $fish_complete_path[-1]
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,41 +1,41 @@
 function _pwd_with_tilde
-  echo $PWD | sed 's|^'$HOME'\(.*\)$|~\1|'
+    echo $PWD | sed 's|^'$HOME'\(.*\)$|~\1|'
 end
 
 function _print_in_color
-  set -l string $argv[1]
-  set -l color  $argv[2]
+    set -l string $argv[1]
+    set -l color  $argv[2]
 
-  set_color $color
-  printf $string
-  set_color normal
+    set_color $color
+    printf $string
+    set_color normal
 end
 
 function _prompt_color_for_status
-  if test $argv[1] -eq 0
-    echo green
-  else
-    echo red
-  end
+    if test $argv[1] -eq 0
+        echo green
+    else
+        echo red
+    end
 end
 
 function fish_prompt
-  set -l last_status $status
+    set -l last_status $status
 
-  _print_in_color "\n" blue
-  if test -z "$TMUX"
-	  _print_in_color (_pwd_with_tilde) blue
-  end
-
-  if test -z "$DIRENV_DIFF"
-    _print_in_color " ❯ " (_prompt_color_for_status $last_status)
-  else
-    if true
-      set -q envprompt; or set envprompt ENV
-      _print_in_color " $envprompt" cyan
-    else
-	    _print_in_color " ENV" yellow
+    _print_in_color "\n" blue
+    if test -z "$TMUX"
+        _print_in_color (_pwd_with_tilde) blue
     end
-    _print_in_color " ❯ " (_prompt_color_for_status $last_status)
-  end
+
+    if test -z "$DIRENV_DIFF"
+        _print_in_color " ❯ " (_prompt_color_for_status $last_status)
+    else
+        if true
+            set -q envprompt; or set envprompt ENV
+            _print_in_color " $envprompt" cyan
+        else
+            _print_in_color " ENV" yellow
+        end
+        _print_in_color " ❯ " (_prompt_color_for_status $last_status)
+    end
 end
