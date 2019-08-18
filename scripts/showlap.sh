d5e7140829080d86abadbcaa0d1ccd1899620ac1
Amos Bird <amosbird@gmail.com>
2019-08-18 22:13:24 +0800

    git update again

---
+++ scripts/showlap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080  --output DP-1 --off --output DP-2 --primary --mode 1920x1080 --pos 0x0 --brightness 1 --set "Broadcast RGB" "Full" --output HDMI-1 --off --output HDMI-2 --off
