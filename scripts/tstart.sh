cd24454c7e3388c4633b1fec5fe91d48788efca1
Amos Bird <amosbird@gmail.com>
2019-10-14 16:11:38 +0800

    tstart fix

--- scripts/tstart.sh
+++ scripts/tstart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export LANG=en_US.UTF-8
-export SHELL
+export SHELL=/home/amos/gentoo/usr/local/bin/fish # for tmux
 tmux -u new -d -s htop htop
 if ! tmux list-sessions | grep -q -F emacs; then
     fuser -k /tmp/emacs.lock # sometimes emacs daemon doesn't quit
