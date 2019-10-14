7e74427f1df90d3aaefcb308fa696835bd85be72
Amos Bird <amosbird@gmail.com>
2019-10-15 04:11:44 +0800

    ssh

--- scripts/tstart.sh
+++ scripts/tstart.sh
@@ -2,6 +2,9 @@
 
 export LANG=en_US.UTF-8
 export SHELL=/home/amos/gentoo/usr/local/bin/fish # for tmux
+if test "$SSH_AUTH_SOCK"; then
+    ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+fi
 tmux -u new -d -s htop htop
 if ! tmux list-sessions | grep -q -F emacs; then
     fuser -k /tmp/emacs.lock # sometimes emacs daemon doesn't quit
