af0c9dd0484ff58991d55d296fc602060cfca071
Amos Bird <amosbird@gmail.com>
2019-10-15 04:13:24 +0800

    ssh fix

--- scripts/tstart.sh
+++ scripts/tstart.sh
@@ -5,6 +5,8 @@ export SHELL=/home/amos/gentoo/usr/local/bin/fish # for tmux
 if test "$SSH_AUTH_SOCK"; then
     ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
 fi
+export SSH_AUTH_SOCK="$HOME/.ssh/ssh_auth_sock"
+
 tmux -u new -d -s htop htop
 if ! tmux list-sessions | grep -q -F emacs; then
     fuser -k /tmp/emacs.lock # sometimes emacs daemon doesn't quit
