2b30cef4492dcb60f297949419706ad8de85c654
Amos Bird <amosbird@gmail.com>
2019-11-07 20:10:20 +0800

    a

--- scripts/gremote.sh
+++ scripts/gremote.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-remote_sock=$(ssh $1 'file=$(/home/amos/gentoo/usr/bin/gpgconf --list-dir agent-socket); rm $file; echo $file')
+ssh $1 '/home/amos/gentoo/usr/bin/gpgconf --create-socketdir; sleep 10;' &
+
+remote_sock=$(ssh $1 '/home/amos/gentoo/usr/bin/gpgconf --create-socketdir; file=$(/home/amos/gentoo/usr/bin/gpgconf --list-dir agent-socket); rm $file; echo $file;')
+
 termite -t $1 -e "ssh -A -t $1 -R $remote_sock:$(gpgconf --list-dir agent-extra-socket) 'env -i HOME=\$HOME TERM=\$TERM USER=\$USER SSH_CONNECTION=\"\$SSH_CONNECTION\" SSH_AUTH_SOCK=\"\$SSH_AUTH_SOCK\" SSH_REMOTE_HOST=\"\$(hostname)\" /home/amos/gentoo/startprefix'"
---
+++ scripts/gtest.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gremote.sh duanxueye@10.61.150.2
