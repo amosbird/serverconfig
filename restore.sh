4789a0bf453587a04fa42d95c61a67d3c03a316f
Amos Bird <amosbird@gmail.com>
2019-11-23 14:31:33 +0800

    Lampson

--- restore.sh
+++ restore.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.config
+mkdir -p /home/amos/.config
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mkdir -p ~/.lc
-ln -sf "$DIR"/lc/config.json "$HOME"/.lc/
+mkdir -p /home/amos/.lc
+ln -sf "$DIR"/lc/config.json /home/amos/.lc/
 
 configs=(
     .globalrc
@@ -37,36 +37,41 @@ configs=(
 )
 
 for config in "${configs[@]}"; do
-    rm -rf "$HOME/$config"
-    ln -sf "$DIR"/"$config" "$HOME"/
+    rm -rf "/home/amos/$config"
+    ln -sf "$DIR/$config" /home/amos/
 done
 
-for dotconfig in $DIR/.config/*; do
-    rm -rf "$HOME/.config/$(basename "$dotconfig")"
-    ln -sf "$dotconfig" "$HOME/.config/"
+for dotconfig in "$DIR/.config/"*; do
+    rm -rf "/home/amos/.config/$(basename "$dotconfig")"
+    ln -sf "$dotconfig" /home/amos/.config/
 done
 
-rm -rf "$HOME/.tmux"
-rm -rf "$HOME/.tmux.conf"
-ln -sf "$DIR/.tmux" "$HOME"/
-ln -sf "$DIR/.tmux/.tmux.conf" "$HOME"/
+rm -rf /home/amos/.tmux
+rm -rf /home/amos/.tmux.conf
+ln -sf "$DIR/.tmux" /home/amos/
+ln -sf "$DIR/.tmux/.tmux.conf" /home/amos/
 
-mkdir -p "$HOME"/.local/share/
-ln -sf "$HOME/gentoo/usr/share/grc" "$HOME"/.local/share/
+mkdir -p /home/amos/.local/share/
+ln -sf /home/amos/gentoo/usr/share/grc /home/amos/.local/share/
 
-mkdir -p -m 700 "$HOME"/.ssh
-cp "$DIR"/id_rsa.pub "$HOME"/.ssh/
+mkdir -p -m 700 /home/amos/.ssh
+cp "$DIR"/id_rsa.pub /home/amos/.ssh/
 
 if [[ $# == 0 ]]; then
-    git clone git@github.com:amosbird/pass-store "$HOME"/.password-store
-    (cd "$HOME"/.password-store && git pull)
+    if [ ! -d /home/amos/.password-store/.git ]; then
+        rm -rf /home/amos/.password-store
+        git clone git@github.com:amosbird/pass-store /home/amos/.password-store
+    fi
+    (cd /home/amos/.password-store && git pull)
 
     sudo cp -r "$DIR"/xkb/* /usr/share/X11/xkb/symbols/
 
     setxkbmap us
 
     gpg --recv-keys 80D430DCBECFEDB4
+    echo -e "5\ny\n" | gpg --command-fd 0 --expert --edit-key 80D430DCBECFEDB4 trust
     gpg --recv-keys C3BFA922206F41DA
+    echo -e "5\ny\n" | gpg --command-fd 0 --expert --edit-key C3BFA922206F41DA trust
 fi
 
 echo 'Restored!'
