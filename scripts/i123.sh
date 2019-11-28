07c43f5716980a20849665839ef8e1aa756eb3db
Amos Bird <amosbird@gmail.com>
2019-11-28 17:57:59 +0800

    nit

--- scripts/i123.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# urxvt -T 10.61.2.123 -e ssh -t 10.61.2.123 /home/amos/scripts/tstart.sh
-# alacritty -t 10.61.2.123 -e ssh -t 10.61.2.123 'bash -l /home/amos/scripts/tstart.sh'
-# st -T 10.61.2.123 -e ssh -t 10.61.2.123 /home/amos/scripts/tstart.sh
-
-# need to installterm first
-termite -t 10.61.2.123 -e "ssh -t 10.61.2.123 /home/amos/scripts/tstart.sh"
