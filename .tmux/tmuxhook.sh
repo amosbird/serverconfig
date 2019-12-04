a80ed21e3e7d9c197428af32cf698835700e6d33
Amos Bird <amosbird@gmail.com>
2019-12-04 14:17:38 +0800

    fix

--- .tmux/tmuxhook.sh
+++ .tmux/tmuxhook.sh
@@ -3,16 +3,16 @@
 if [[ $1 -eq 1 ]]; then
     case "$2" in
     emacs)
-        tmux source -t emacs ~/.tmux/.tmux.conf.emacs
+        tmux source ~/.tmux/.tmux.conf.emacs
         ;;
     htop)
-        tmux source -t htop ~/.tmux/.tmux.conf.htop
+        tmux source ~/.tmux/.tmux.conf.htop
         ;;
     amos)
-        tmux source -t amos ~/.tmux/.tmux.conf.amos
+        tmux source ~/.tmux/.tmux.conf.amos
         ;;
     gui)
-        tmux source -t gui ~/.tmux/.tmux.conf.gui
+        tmux source ~/.tmux/.tmux.conf.gui
         ;;
     esac
 elif [[ $1 -eq 2 ]]; then
@@ -27,4 +27,6 @@ elif [[ $1 -eq 3 ]]; then
     if [[ $2 == htop ]]; then
         tmux copy-mode
     fi
+elif [[ $1 -eq 4 ]]; then
+    tmux source ~/.tmux/.tmux.conf.amos
 fi
