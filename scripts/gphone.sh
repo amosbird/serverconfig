5eabbd0a60cea7b964a194c8e700e5773c360044
Amos Bird <amosbird@gmail.com>
2020-01-06 22:31:50 +0800

    gphone

--- scripts/gphone.sh
+++ scripts/gphone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # don't forward for myself
-termite -t phone -e "ssh -t root@phone 'env -i TERM=\$TERM SSH_CONNECTION=\"\$SSH_CONNECTION\" /data/gentoo64/startprefix'"
+termite -t phone -e "adb shell -t su -c source /data/gentoo64/startprefix"
