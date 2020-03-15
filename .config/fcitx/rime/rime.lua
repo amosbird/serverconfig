0f73a216df5e5dadf7943da8735fabf1a7506b0c
Amos Bird <amosbird@gmail.com>
2020-03-16 02:04:45 +0800

    i

--- .config/fcitx/rime/rime.lua
+++ .config/fcitx/rime/rime.lua
@@ -68,4 +68,10 @@ single_char_filter = require("single_char")
 -- 详见 `lua/reverse.lua`
 reverse_lookup_filter = require("reverse")
 
-cloud = require("cloud")
+--- 百度云拼音，Control+t 为云输入触发键
+--- 使用方法：
+--- 将 "lua_translator@baidu_translator" 和 "lua_processor@baidu_processor"
+--- 分别加到输入方案的 engine/translators 和 engine/processors 中
+local baidu = require("trigger")("Control+t", require("baidu"))
+baidu_translator = baidu.translator
+baidu_processor = baidu.processor
