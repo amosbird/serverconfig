5fb7a53c2824a5b06518fbb2339c236fe26d7b3f
Amos Bird <amosbird@gmail.com>
2020-03-16 02:04:45 +0800

    i

---
+++ .config/fcitx/rime/lua/trigger.lua
@@ -0,0 +1,32 @@
+local function make(trig_key, trig_translator)
+    local flag = false
+
+    local function processor(key, env)
+        local kAccepted = 1
+        local kNoop = 2
+        local engine = env.engine
+        local context = engine.context
+
+        if key:repr() == trig_key then
+            if context:is_composing() then
+                flag = true
+                context:refresh_non_confirmed_composition()
+                return kAccepted
+            end
+        end
+
+        return kNoop
+    end
+
+    local function translator(input, seg, env)
+        -- local s_orig = context:get_commit_text()
+        if flag then
+            flag = false
+            trig_translator(input, seg, env)
+        end
+    end
+
+    return {processor = processor, translator = translator}
+end
+
+return make
