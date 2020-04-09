19f46aa515251cfe7bc8626584d98e62e7d368e7
Amos Bird <amosbird@gmail.com>
2020-04-09 16:30:53 +0800

    mm

---
+++ scripts/showdiscord.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+workspace=$(bspc query -D -d focused --names)
+if pgrep Discord >/dev/null; then
+    id=$(head -1 /tmp/discord)
+    if [ -z "$id" ]; then
+        exit 0
+    fi
+    if bspc query -N -n focused | grep -q "$(bspc query -N -n "$id")"; then
+        bspc node "$id" -g hidden -f
+    else
+        bspc node "$id" --to-desktop "$workspace"
+        bspc node "$id" -t floating
+        bspc node "$id" -g hidden=off -f
+    fi
+    wh=($(xrandr --current | perl -ne 'if (/primary/) {@x=split; $x[3] =~ /(\d+)x(\d+)/; print $1." ".$2}'))
+    w=${wh[0]}
+    h=${wh[1]}
+    x=$((w / 8))
+    y=80
+    w=$((w * 3 / 4))
+    h=$((h - 140))
+    xdo move -x $x -y $y "$id"
+    xdo resize -w $w -h $h "$id"
+    bspc node "$id" -l above
+else
+    rm /tmp/discord
+    # env FONTCONFIG_FILE=~/.config/tgfonts.conf
+    bash -c "discord &"
+fi
