74a4d13ad34ab5d622d9c3dcfaf93d5369fb0c38
Amos Bird <amosbird@gmail.com>
2020-04-11 00:08:09 +0800

    htop fix

--- .tmux/tmuxhook.sh
+++ .tmux/tmuxhook.sh
@@ -16,13 +16,13 @@ elif [[ $1 -eq 2 ]]; then
     if [[ $2 == emacs ]]; then
         tmux send f12
     elif [[ $2 == htop ]]; then
-        tmux send -X cancel
+        tmux run-shell "/home/amos/scripts/tmuxsuspend USR2"
     else
         /home/amos/scripts/setcursor.sh $(tmux display -p "#{pane_tty}")
     fi
 elif [[ $1 -eq 3 ]]; then
     if [[ $2 == htop ]]; then
-        tmux copy-mode
+        tmux run-shell "/home/amos/scripts/tmuxsuspend USR1"
     fi
 elif [[ $1 -eq 4 ]]; then
     tmux source ~/.tmux/.tmux.conf.amos
