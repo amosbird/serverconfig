3de8fa9997e43ace48c65e53e61e26e5ed1e26d2
Amos Bird <amosbird@gmail.com>
2020-04-12 05:10:13 +0800

    n

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -2278,7 +2278,7 @@ representation of `NUMBER' is smaller."
   "Copy the current buffer's path to the kill ring."
   (interactive)
   (if-let* ((filename (or buffer-file-name (bound-and-true-p list-buffers-directory))))
-      (message (kill-new (abbreviate-file-name filename)))
+      (shell-command! (format "fcp %s" (abbreviate-file-name filename)))
     (error "Couldn't find filename in current buffer")))
 
 (defun +amos/yank-buffer-filename-nondir ()
