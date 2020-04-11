57d644e978a6041ba16fa83e7aedea3ee2a7148a
Amos Bird <amosbird@gmail.com>
2020-04-11 17:26:08 +0800

    w

---
+++ autokey/discord/backward.py
@@ -0,0 +1,5 @@
+# Enter script code
+
+# keyboard.send_key("<ctrl>+<right>")
+keyboard.send_key("<left>")
+# keyboard.fake_keypress("<ctrl>+<right>")
---
+++ autokey/discord/jump.py
@@ -0,0 +1,5 @@
+# Enter script code
+
+# keyboard.send_key("<ctrl>+<right>")
+keyboard.send_keys("<ctrl>+k")
+# keyboard.send_keys("<shift>+<end><delete>")
