197d56d8ae6889fb58bbb1e2c53a3603a5337c17
Amos Bird <amosbird@gmail.com>
2020-07-12 00:23:57 +0800

    hh

--- xkeysnail.py
+++ xkeysnail.py
@@ -9,13 +9,8 @@ define_timeout(1)
 # # [Multipurpose modmap] Give a key two meanings. A normal key when pressed and
 # # released, and a modifier key when held down with another key. See Xcape,
 # # Carabiner and caps2esc for ideas and concept.
-define_multipurpose_modmap(
-    # Enter is enter when pressed and released. Control when held down.
+define_conditional_multipurpose_modmap(re.compile("discord|TelegramDesktop|Wine"),
     {Key.LEFT_CTRL: [Key.ESC, Key.LEFT_CTRL]}
-
-    # Capslock is escape when pressed and released. Control when held down.
-    # {Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]
-    # To use this example, you can't remap capslock with define_modmap.
 )
 
 # # [Conditional multipurpose modmap] Multipurpose modmap in certain conditions,
