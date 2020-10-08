7f4f9cccbff44448b9f086497e8962db2d43b2cb
Amos Bird <amosbird@gmail.com>
2020-10-08 19:18:39 +0800

    d

--- scripts/showtele.sh
+++ scripts/showtele.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 workspace=$(bspc query -D -d focused --names)
-# if pgrep telegram-deskto >/dev/null; then
-if pgrep Telegram >/dev/null; then
+if pgrep telegram-deskto >/dev/null; then
+# if pgrep Telegram >/dev/null; then
     id=$(head -1 /tmp/telegram)
     if [ -z "$id" ]; then
         exit 0
@@ -27,6 +27,6 @@ if pgrep Telegram >/dev/null; then
 else
     rm /tmp/telegram
     # env FONTCONFIG_FILE=~/.config/tgfonts.conf
-    # bash -c "telegram-desktop &"
-    bash -c "Telegram &"
+    bash -c "telegram-desktop &"
+    # bash -c "Telegram &"
 fi
