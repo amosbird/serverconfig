866c03ae8b5516165192c116d8395a22b308c79f
Amos Bird <amosbird@gmail.com>
2022-11-08 01:10:25 +0800

    Fcitx5 doesn't work well with xcb notification

--- .local/share/fcitx5/lua/notification/notification.lua
+++ .local/share/fcitx5/lua/notification/notification.lua
@@ -4,15 +4,15 @@ fcitx.watchEvent(fcitx.EventType.InputMethodActivated, "activated")
 fcitx.watchEvent(fcitx.EventType.InputMethodDeactivated, "deactivated")
 
 function activated(event_name)
-  if event_name == "rime" then
-    io.popen("echo 'a' | nc -w 0 -U /tmp/caps-indicator.socket")
-  else
-    io.popen("echo 'd' | nc -w 0 -U /tmp/caps-indicator.socket")
-  end
-  return false
+	#  if event_name == "rime" then
+	#    io.popen("echo 'a' | nc -w 0 -U /tmp/caps-indicator.socket")
+	#  else
+	#    io.popen("echo 'd' | nc -w 0 -U /tmp/caps-indicator.socket")
+	#  end
+	#  return false
 end
 
 function deactivated(event_name)
-  io.popen("echo 'd' | nc -w 0 -U /tmp/caps-indicator.socket")
-  return false
+	#  io.popen("echo 'd' | nc -w 0 -U /tmp/caps-indicator.socket")
+	#  return false
 end
