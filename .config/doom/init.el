d338782acf74b1221ffd2d0cafe84dab4a90f991
Amos Bird <amosbird@gmail.com>
2024-04-17 16:59:37 +0800

    scratch in text

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -336,6 +336,7 @@
 (setq word-wrap-by-category t)
 (setq next-error-recenter '(4))
 (setq consult-ripgrep-args "rg --null --line-buffered --color=never --max-columns=1000 --path-separator / --smart-case --no-heading --with-filename --line-number")
+(setq initial-major-mode 'text-mode)
 
 (after! core-keybinds
   (add-to-list 'doom-evil-state-alist '(?s . sticky))
