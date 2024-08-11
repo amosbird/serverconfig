c20d003f704d320b0098ab38549365e2080da7f0
Amos Bird <amosbird@gmail.com>
2024-08-11 23:46:18 +0800

    kitty pager done

--- .config/nvim/lua/kitty+page.lua
+++ .config/nvim/lua/kitty+page.lua
@@ -1,4 +1,19 @@
+vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
+    group = vim.api.nvim_create_augroup("KittySetVarVimEnter", { clear = true }),
+    callback = function()
+        io.stdout:write("\x1b]1337;SetUserVar=in_editor=MQo\007")
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+    group = vim.api.nvim_create_augroup("KittyUnsetVarVimLeave", { clear = true }),
+    callback = function()
+        io.stdout:write("\x1b]1337;SetUserVar=in_editor\007")
+    end,
+})
+
 return function(INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN, SEARCH)
+  -- use :messages to view log
   print('kitty sent:', INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN, SEARCH)
 
   local function setOptions()
@@ -26,16 +41,28 @@ return function(INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN, SEARCH)
   end
   local term_buf = vim.api.nvim_create_buf(true, false);
   local term_io = vim.api.nvim_open_term(term_buf, {})
-  vim.api.nvim_buf_set_keymap(term_buf, 'v', '<ESC>', 'y<Cmd>q<CR>', { })
-  vim.api.nvim_buf_set_keymap(term_buf, 'n', '<ESC>', '<Cmd>q<CR>', { })
+
+  vim.api.nvim_buf_set_keymap(term_buf, 'v', '<ESC>', 'y<Cmd>lua vim.defer_fn(function() vim.cmd("quit") end, 10)<CR>', { noremap = true })
+  vim.api.nvim_buf_set_keymap(term_buf, 'n', '<ESC>', '<Cmd>q<CR>', { noremap = true })
+  vim.keymap.set('n', '<M-u>', function() pcall(vim.cmd, 'normal! n') end,
+                 { buffer = term_buf, noremap = true, silent = true })
+  vim.keymap.set('n', '<M-n>', function() pcall(vim.cmd, 'normal! N') end,
+                 { buffer = term_buf, noremap = true, silent = true })
+
+  -- local function search_next_from_cmdline()
+  --   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-c>', true, true, true), 'n', true)
+  --   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('N', true, true, true), 'n', true)
+  --   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('?<Up>', true, true, true), 'n', true)
+  -- end
+  -- vim.keymap.set('c', '<M-s>', search_next_from_cmdline, { noremap = true, silent = true })
+  vim.api.nvim_buf_set_keymap(term_buf, 'c', '<M-s>', '<C-t>', { noremap = true })
+  vim.api.nvim_buf_set_keymap(term_buf, 'c', '<M-u>', '<C-t>', { noremap = true })
+  vim.api.nvim_buf_set_keymap(term_buf, 'c', '<M-n>', '<C-g>', { noremap = true })
+
   vim.api.nvim_buf_set_keymap(term_buf, 'n', 'u', '<C-u>', { noremap = true })
   vim.api.nvim_buf_set_keymap(term_buf, 'n', 'd', '<C-d>', { noremap = true })
   vim.api.nvim_buf_set_keymap(term_buf, 'n', 'g', 'gg', { noremap = true, nowait = true })
 
-  -- vim.api.nvim_buf_set_keymap(term_buf, 'n', '<ESC>', '<Cmd>q<CR>', { })
-  -- vim.api.nvim_buf_set_keymap(term_buf, 'n', '<ESC>', '<Cmd>q<CR>', { })
-  -- vim.api.nvim_buf_set_keymap(term_buf, 'n', '<ESC>', '<Cmd>q<CR>', { })
-  -- vim.api.nvim_buf_set_keymap(term_buf, 'n', '<ESC>', '<Cmd>q<CR>', { })
   local group = vim.api.nvim_create_augroup('kitty+page', {})
 
   function UpdateTabline()
@@ -64,6 +91,19 @@ return function(INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN, SEARCH)
   vim.opt.hlsearch = true
   vim.opt.ignorecase = true
   vim.opt.smartcase = true
+  vim.opt.wrapscan = false
+
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
 
   vim.cmd([[
     augroup UpdateTablineOnSearch
@@ -80,19 +120,31 @@ return function(INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN, SEARCH)
   UpdateTabline()
 
   local setCursor = function()
-    vim.api.nvim_feedkeys(tostring(INPUT_LINE_NUMBER) .. [[ggzt]], 'n', true)
     local line = vim.api.nvim_buf_line_count(term_buf)
-    if (CURSOR_LINE <= line) then
+    local start_line = INPUT_LINE_NUMBER
+    local scroll_line = line - 1
+    if (CURSOR_LINE < line) then
       line = CURSOR_LINE
+      start_line = start_line + 1
+      scroll_line = line - 2
     end
-    vim.api.nvim_feedkeys(tostring(line - 1) .. [[j]], 'n', true)
+    vim.api.nvim_feedkeys(tostring(start_line) .. [[ggzt]], 'n', true)
+    if (scroll_line > 0) then
+      vim.api.nvim_feedkeys(tostring(scroll_line) .. [[j]], 'n', true)
+    end
     vim.api.nvim_feedkeys([[0]], 'n', true)
     vim.api.nvim_feedkeys(tostring(CURSOR_COLUMN - 1) .. [[l]], 'n', true)
+
     vim.cmd('highlight MsgArea guibg=#00346E')
     vim.cmd('highlight TabLineFill guibg=#00346E')
+    vim.cmd('highlight Search guibg=#70C0BA guifg=#000000')
+    vim.cmd('highlight IncSearch guibg=#C397D8 guifg=#000000')
+    vim.cmd('highlight CurSearch guibg=#C397D8 guifg=#000000')
 
-    if (SEARCH == 1) then
+    if (SEARCH == 's') then
       vim.api.nvim_feedkeys('?', 'n', true)
+    elseif (SEARCH == 'u') then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('k?❯<CR>', true, false, true), 'n', true)
     end
   end
 
