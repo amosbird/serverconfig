208f8d315fe73518d9799745e4c9a94440b01b0a
Amos Bird <amosbird@gmail.com>
2025-02-05 23:19:28 +0800

    better gstart.sh

--- scripts/gstart.sh
+++ scripts/gstart.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+pkill -f "kitty -o allow_remote_control=yes --listen-on unix:/tmp/kitty_sock -T local"
+
 kitty -o allow_remote_control=yes --listen-on unix:/tmp/kitty_sock -T local "$HOME/scripts/tstart.sh" local
