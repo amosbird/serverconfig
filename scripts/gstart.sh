6bb1e5bd617cbfd476297c3adaf4aaf1fdec29ab
Amos Bird <amosbird@gmail.com>
2026-04-20 13:16:30 +0800

    Proper kill

--- scripts/gstart.sh
+++ scripts/gstart.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-pkill -f "kitty -o allow_remote_control=yes --listen-on unix:/tmp/kitty_sock -T local" || :
+if [[ "$(hostname)" == "abx1gen3" ]]; then
+    killwork
+else
+    pkill -f "kitty -o allow_remote_control=yes --listen-on unix:/tmp/kitty_sock -T local" || :
+fi
 
 kitty -o allow_remote_control=yes --listen-on unix:/tmp/kitty_sock -T local "$HOME/scripts/tstart.sh" local &
 
