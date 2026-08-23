6822d6c8d27ce6f5c8bd5315cc5593aa9c7958ba
Amos Bird <amosbird@gmail.com>
2026-08-23 13:59:17 +0800

    update gwork

--- scripts/gstart.sh
+++ scripts/gstart.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ "$(hostname)" == "abx1gen3" ]]; then
-    killwork
-else
-    pkill -f "kitty -o allow_remote_control=yes --listen-on unix:/tmp/kitty_sock -T local" || :
-fi
+killwork
+
 rm -f /tmp/kitty_sock
 
 kitty -o allow_remote_control=yes --listen-on unix:/tmp/kitty_sock -T local "$HOME/scripts/tstart.sh" local &
